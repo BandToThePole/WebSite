c5c170908ae539c618a1e71de81ea810e1b79854
Thomas Denney <me@thomasdenney.co.uk>
2017-03-19 15:15:45 +0000

    Trying something simpler...

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
-var express = require('express');
-var app = express()
-
-/*var Connection = require('tedious').Connection;
-// Very important that the password never appears in git
-var config = {
-  userName: "oxfordCSteam11Y16",
-  password: process.env.BTP_PASSWORD,
-  server: "bandtothepoledb.database.windows.net",
-  options: {
-    encrypt: true,
-    database: "bandtothepoledb"
-  }
-};*/
-
-var log = [];
-
-/*var connection = new Connection(config);
-connection.on('connect', function(err) {
-  log.push(`connect: ${JSON.stringify(err, null, "  ")}`);
-});
-connection.on('errorMessage', function(err) {
-  log.push(`errorMessage: ${JSON.stringify(err, null, "  ")}`)
-});
-connection.on('error', function(err) {
-  log.push(`error: ${JSON.stringify(err, null, "  ")}`);
-});*/
-
+var http = require('http')
 var port = process.env.PORT || 1337;
-app.get('/', function (req, res) {
-  res.send(JSON.stringify(log));
-});
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
