35cfab936bed3426795fcd018e3b79ee5bdc4af0
Thomas Denney <me@thomasdenney.co.uk>
2017-03-19 15:19:57 +0000

    Actually add dependencies

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-var http = require('http')
+var express = require('express');
+var app = express();
+
+/*var Connection = require('tedious').Connection;
+// Very important that the password never appears in git
+var config = {
+  userName: "oxfordCSteam11Y16",
+  password: process.env.BTP_PASSWORD,
+  server: "bandtothepoledb.database.windows.net",
+  options: {
+    encrypt: true,
+    database: "bandtothepoledb"
+  }
+};*/
+
+var log = [];
+
+/*var connection = new Connection(config);
+connection.on('connect', function(err) {
+  log.push(`connect: ${JSON.stringify(err, null, "  ")}`);
+});
+connection.on('errorMessage', function(err) {
+  log.push(`errorMessage: ${JSON.stringify(err, null, "  ")}`)
+});
+connection.on('error', function(err) {
+  log.push(`error: ${JSON.stringify(err, null, "  ")}`);
+});*/
+
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+app.get('/', function (req, res) {
+  res.send(JSON.stringify(log));
+});
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
