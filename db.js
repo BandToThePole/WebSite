26357d4396ddb4a50bf9d088ac5a147765e30bc4
Thomas Denney <me@thomasdenney.co.uk>
2017-04-24 16:46:05 +0100

    Moved all API serving to a separate file

---
+++ db.js
@@ -0,0 +1,35 @@
+var ConnectionPool = require('tedious-connection-pool');
+
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+var log = [];
+
+var poolConfig = {
+    min: 2,
+    log: function(message){log.push(message)}
+};
+
+// Very important that the password never appears in git
+var config = {
+    userName: "oxfordCSteam11Y16",
+    password: process.env.BTP_PASSWORD,
+    server: "bandtothepoledb.database.windows.net",
+    options: {
+        encrypt: true,
+        database: "bandtothepoledb"
+    }
+};
+
+var pool = new ConnectionPool(poolConfig,config);
+
+pool.on('error',function(err){
+    log.push(err);
+});
+
+function getLog() {
+    return log;
+}
+
+exports.getLog = getLog;
+exports.pool = pool;
